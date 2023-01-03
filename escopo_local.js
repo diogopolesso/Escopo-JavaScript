9a97ed3fd871b19f2103975e6e4c41996e579c81
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-03 13:11:08 -0300

    Escopo Local

--- escopo_local.js
+++ escopo_local.js
@@ -1,1 +1,16 @@
+// Escopo Local - Uma variável se torna local quando ela é declarada dentro de uma função, de tal maneira a qual ela somente estará acessível dentro dessa função.
 
+var nome = "Diogo";
+
+// (ESCOPO
+function myName() {
+  var myLastName = "Polesso";
+  console.log('Meu Nome é ' + nome + ' ' + myLastName);
+}
+// LOCAL)
+
+console.log('Meu nome é ' + nome + ' ' + myLastName);
+myName();
+
+
+// O Escopo local se delimita a chave de abertura e fechamento da função myName.
